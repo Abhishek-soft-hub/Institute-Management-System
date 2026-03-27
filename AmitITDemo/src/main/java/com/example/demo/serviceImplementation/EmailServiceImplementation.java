e6a15622d5c8b4771422d237e5016c9ca8d1f7f2
Abhishek-soft-hub <236396774+Abhishek-soft-hub@users.noreply.github.com>
2026-03-27 15:13:25 +0530

    update

--- AmitITDemo/src/main/java/com/example/demo/serviceImplementation/EmailServiceImplementation.java
+++ AmitITDemo/src/main/java/com/example/demo/serviceImplementation/EmailServiceImplementation.java
@@ -1,10 +1,29 @@
 package com.example.demo.serviceImplementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmailServiceImplementation {
+import com.example.demo.repository.EmailOtpServiceRepository;
+import com.example.demo.service.EmailService;
 
+@Service
+public class EmailServiceImplementation implements EmailService {
+
+	@Autowired
+	EmailOtpServiceRepository emailOtpServiceRepository;
 	
-	
+	Email
+
+	@Override
+	public String sendOtp(String email) {
+
+		return null;
+	}
+
+	@Override
+	public String verifyEmailOtp(String email, String otp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
